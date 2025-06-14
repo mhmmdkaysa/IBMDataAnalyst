f42e8a7561fb5aded42ffba7172f8f42318dbbfa
mhmmdkaysa <110692258+mhmmdkaysa@users.noreply.github.com>
2025-06-14 16:09:11 +0700

    Update accessing_databases.py

--- Python Data Analysis/accessing_databases.py
+++ Python Data Analysis/accessing_databases.py
@@ -1,1 +1,16 @@
+# Writing codes using DB-API
+from dmodule import connect
 
+# Create connection object
+connection = connect('databasename', 'username', 'pswd')
+
+# Create a cursor object
+cursor = connection.cursor()
+
+# Run queries
+cursor.execute('select * from mytable')
+result = cursor.fetchall()
+
+# Free resources
+cursor.close()
+connection.close()
